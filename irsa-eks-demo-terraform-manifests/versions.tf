5209a9a5dba859bfa2cd02e3efa3c8988dbd4c70
Alexis Chow-Yuen <alexis.chow-yuen@epita.fr>
2023-03-04 15:32:00 +0100

    [ADD][IRSA] Add backend

--- irsa-eks-demo-terraform-manifests/versions.tf
+++ irsa-eks-demo-terraform-manifests/versions.tf
@@ -10,4 +10,13 @@ terraform {
         version = "~>2.7"
       }
     }
+
+    backend "s3" {
+      bucket = "terraform-on-aws-eks-acy"
+      key    = "dev/irsa-demo/terraform.tfstate"
+      region = "us-east-1"
+
+      # For lock state
+      dynamodb_table = "dev-irsa-demo"
+    }
 }
